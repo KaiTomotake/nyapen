db996028b09184b02d7a3ab6a318fa7842b0225b
KaiTomotake <kaitomotake@gmail.com>
2026-02-13 11:30:14 +0900

    add tests directory

---
+++ tests/hw.rs
@@ -0,0 +1,33 @@
+use nyapen::prelude::*;
+use nyapen::primitive::{lit, re};
+
+#[derive(Debug, Clone, PartialEq)]
+enum Hw {
+    Hello,
+    World,
+    Comma,
+}
+
+#[test]
+fn hw_complete() {
+    assert_eq!(
+        lit("Hello")
+            .map(|_, _| Hw::Hello)
+            .then(lit(",").map(|_, _| Hw::Comma).opt())
+            .then(lit("World").map(|_, _| Hw::World).opt())
+            .map(|m, _| match m {
+                Some((Some((hello, comma)), world)) => (hello.unwrap(), comma, world),
+                _ => panic!("unexpected parse structure in hw_complete"),
+            })
+            .repeated()
+            .eoi()
+            .parse(" Hello , World Hello Hello", Some(re("\\s+").unwrap()))
+            .unwrap()
+            .mapped,
+        Some(vec![
+            (Hw::Hello, Some(Hw::Comma,), Some(Hw::World,),),
+            (Hw::Hello, None, None,),
+            (Hw::Hello, None, None,),
+        ],)
+    );
+}
